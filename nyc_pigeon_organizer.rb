2e30ebdc73e62859275142af78a54a60d2ec49e5
Aaron Amodt <aaron@amodttech.com>
2020-10-20 18:58:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,14 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
-      binding.pry
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+          binding.pry
+      end
     end
-
     final_array[k] = v
-
-end
+  end
 binding.pry 
-
 end
+
